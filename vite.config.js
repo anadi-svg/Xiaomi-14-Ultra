5c5861207fccd7e7424c4e3bf5bcf2d4e51b5717
anadi-svg <anadidwivedi@gmail.com>
2024-03-11 02:28:45 +0530

    legacy install

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import legacy from '@vitejs/plugin-legacy'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+
+  plugins: [
+    react(),
+    viteSingleFile(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+
 });
